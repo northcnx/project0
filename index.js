421d6adee884dbc79f884ed6b8469e362e515625
northcnx <northcnx@gmail.com>
2026-03-27 16:15:17 +0700

    f

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// ไฟล์นี้คือ Startup File (ไฟล์เปิดเซิฟเวอร์หลัก)
-// ใช้สำหรับตั้งค่าในโฮสติ้งจำพวก cPanel, Plesk หรืองานที่ต้องชี้ให้รันไฟล์ .js เพื่อเปิดระบบ
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
-
 const dev = process.env.NODE_ENV !== 'production'
-// เพื่อกันการล่มจากการนำชื่อโดเมนมาใส่ผิดที่ กรุณาปล่อยให้เป็น '0.0.0.0' เพื่อรันผ่านทุก Network Interface แบบสากล
-const hostname = '0.0.0.0'
+const hostname = 'localhost'
 const port = process.env.PORT || 3000
-
-// เมื่อรันไฟล์นี้ ระบบ Next.js จะถูกปลุกให้ทำงาน
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
